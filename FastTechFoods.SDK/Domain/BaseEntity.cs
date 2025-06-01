826e97d36653a380903749b3a90ff30a96fc9426
Erick Goldberg <erick_goldberg@hotmail.com>
2025-06-01 09:57:00 -0300

    [Feat] Add domain base

---
+++ FastTechFoods.SDK/Domain/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace FastTechFoods.SDK.Domain
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public DateTime CreatedAt { get; } = DateTime.Now;
+        public DateTime UpdatedAt { get; protected set; } = DateTime.Now;
+    }
+}
